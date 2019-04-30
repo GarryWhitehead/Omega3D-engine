60af037b23a482603f99a15532bb3430cec6387a
Garry Whitehead <garry@kudan.eu>
2019-04-30 13:57:47 +0100

    Added render and new instance commands to skybox - work in progress

--- OmegaEngine/Rendering/RenderableTypes/Skybox.h
+++ OmegaEngine/Rendering/RenderableTypes/Skybox.h
@@ -9,9 +9,30 @@ namespace OmegaEngine
 
 	public:
 
+		struct SkyboxInstance
+		{
+			// vertex and index buffer memory info for the cube
+			VulkanAPI::Buffer vertex_buffer;
+			VulkanAPI::Buffer index_buffer;
+			uint32_t index_count = 0;
+
+			// the factor which the skybox will be blurred by
+			float blur_factor = 0.0f;
+		};
+
 		RenderableSkybox();
 		~RenderableSkybox();
+		
+		// used to get the address of this instance
+		void* get_handle() override
+		{
+			return this;
+		}
 
+		void render(VulkanAPI::SecondaryCommandBuffer& cmd_buffer, 
+					void* instance_data,
+					RenderInterface* render_interface) override;
+					
 	private:
 
 	};
