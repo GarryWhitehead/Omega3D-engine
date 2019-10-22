65bb5986b63ae6adcf5c485f9e9f0b91dad86075
Garry Whitehead <garry@kudan.eu>
2019-10-22 17:50:54 +0100

    Added faces into the calcs

--- OmegaEngine/ImageUtils/Image2D.h
+++ OmegaEngine/ImageUtils/Image2D.h
@@ -25,16 +25,6 @@ struct Colour4
 };
 
 template <typename pixelType>
-class ImageCube2D : public Image2D
-{
-
-public:
-	
-private:
-
-};
-
-template <typename pixelType>
 class Image2D
 {
 public:
@@ -80,38 +70,31 @@ public:
 	}
 
 	/**
-	*  Use this if you don't wnat to move a image across but need to manipulate in someway.
-	* You need to ensure that the image isn;t destroyed whilst still associated with this class.
-	* @param image Pointer to an already allocated portioin of memory where the image is held
-	* @param width The width of the image in pixels
-	* @param height The height of the image in pixels
-	* @param channels The number of channels the image contains (i.e. rgba = 4)
-	*/
-	void setData(pixelType* image, uint32_t width, uint32_t height, uint8_t channels, uint8_t mipCount = 1)
-	{
-		assert(image);
-		data = image;
-		this->width = width;
-		this->height = height;
-		this->channels = channels;
-		this->mipCount = mipCount;
-		this->size = width * height * channels * faces;
-	}
-
-	/**
 	* Allocates a chunk of memory as sepcified by the dimensions of the image
 	* @param width The width of the image in pixels
 	* @param height The height of the image in pixels
 	* @param channels The number of channels the image contains (i.e. rgba = 4)
 	*/
-	void reserve(uint32_t width, uint32_t height, uint8_t channels)
+	void reserve(uint32_t width, uint32_t height, uint8_t channels, uint8_t mipCount)
 	{
-		data = new pixelType[width * height * channels];
-		assert(data);
 		this->width = width;
 		this->height = height;
 		this->channels = channels;
-		this->size = width * height * channels;
+        this->mipLevels = mipCount;
+        
+        // accumulate size for each mip map
+        size_t totalSize = 0;
+        for (uint8_t i = 0; i < mipLevels; ++i)
+        {
+            uint32_t mipWidth = width >> i;
+            uint32_t mipHeight = height >> i;
+            totalSize += mipWidth * mipHeight * channels * faces;
+        }
+        this->size = totalSize;
+        
+        // allocate image memory
+        data = new pixelType[totalSize];
+        assert(data);
 	}
 
 	/**
@@ -129,15 +112,74 @@ public:
 	* @param x The x coord in pixels
 	* @param y The y coord in pixels
 	* @param channel The channel to retrive (0 = red; 1 = green; 2 = blue; 3 = alpha)
+    * @param The face which to extract data from. This must be a cube image to be used
 	*/
-	pixelType getPixel(uint32_t x, uint32_t y, uint8_t channel)
+	pixelType getPixel(uint32_t x, uint32_t y, uint8_t channel, uint8_t face = 1)
 	{
 		assert(data);
 		assert(x <= width);
 		assert(y <= height);
 		assert(channel <= channels);
-		return data[(x * channels) + channel + (y * width)];
+        if (face > 1 && type != ImageType::Cube)
+        {
+            LOGGER_ERROR("Trying to use a Image2D class a cube type when it has been initialised as a normal texture");
+            return {};
+        }
+
+        size_t startPos = width * height * channels * (face - 1);
+        size_t pixelPos = ((x * channels) + channel + (y * width)) + startPos;
+		return data[pixelPos];
 	}
+    
+    /**
+    * Retrieves a rgb value from a specific area of the image.
+    * @param x The x coord in pixels
+    * @param y The y coord in pixels
+    * @param The face which to extract data from. This must be a cube image to be used
+    */
+    Colour3 getPixel(uint32_t x, uint32_t y, uint8_t face = 1)
+    {
+        assert(data);
+        assert(x <= width);
+        assert(y <= height);
+        assert(channels == 3);
+        
+        if (face > 1 && type != ImageType::Cube)
+        {
+            LOGGER_ERROR("Trying to use a Image2D class a cube type when it has been initialised as a normal texture");
+            return {};
+        }
+        
+        size_t startPos = width * height * channels * (face - 1);
+        size_t pixelPos = ((x * channels) + (y * width)) + startPos;
+        
+        return {data[pixelPos], data[pixelPos + 1], data[pixelPos + 2]};
+    }
+    
+    /**
+    * Retrieves a rgba value from a specific area of the image.
+    * @param x The x coord in pixels
+    * @param y The y coord in pixels
+    * @param The face which to extract data from. This must be a cube image to be used
+    */
+    Colour4 getPixel(uint32_t x, uint32_t y, uint8_t face = 1)
+    {
+        assert(data);
+        assert(x <= width);
+        assert(y <= height);
+        assert(channels == 3);
+        
+        if (face > 1 && type != ImageType::Cube)
+        {
+            LOGGER_ERROR("Trying to use a Image2D class a cube type when it has been initialised as a normal texture");
+            return {};
+        }
+        
+        size_t startPos = width * height * channels * (face - 1);
+        size_t pixelPos = ((x * channels) + (y * width)) + startPos;
+        
+        return {data[pixelPos], data[pixelPos + 1], data[pixelPos + 2]};
+    }
 
 	/**
 	* Sets a pixel to the specified value at the coord point indicated. 
@@ -145,26 +187,22 @@ public:
 	* @param y The y coord in pixels
 	* @param channel The channel to retrive (0 = red; 1 = green; 2 = blue; 3 = alpha)
 	*/
-	void setPixel(pixelType pixel, uint32_t x, uint32_t y, uint8_t channel)
+	void setPixel(pixelType pixel, uint32_t x, uint32_t y, uint8_t channel, uint8_t face = 1)
 	{
 		assert(x <= width);
 		assert(y <= height);
 		assert(channel <= channels);
-		data[(x * channels) + channel + (y * width)] = pixel;
-	}
-
-	/**
-	* Sets a pixel to the specified value at the coord point indicated. 
-	* @param x The x coord in pixels
-	* @param y The y coord in pixels
-	* @param channel The channel to set (0 = red; 1 = green; 2 = blue; 3 = alpha)
-	*/
-	void setPixel(pixelType pixel, uint32_t x, uint32_t y, uint8_t channel)
-	{
-		assert(x <= width);
-		assert(y <= height);
-		assert(channel <= channels);
-		data[(x * channels) + channel + (y * width)] = pixel;
+        
+        if (face > 1 && type != ImageType::Cube)
+        {
+            LOGGER_ERROR("Trying to use a Image2D class a cube type when it has been initialised as a normal texture");
+            return {};
+        }
+        
+        size_t startPos = width * height * channels * (face - 1);
+        size_t pixelPos = ((x * channels) + channel + (y * width)) + startPos;
+        assert(pixelPos < size);
+		data[pixelPos] = pixel;
 	}
 
 	/**
@@ -173,15 +211,25 @@ public:
 	* @param y The y coord in pixels
 	* @param pixels A rgb definition of the pixel values
 	*/
-	void setPixel(pixelType pixel, uint32_t x, uint32_t y, Colour3& pixels)
+	void setPixel(pixelType pixel, uint32_t x, uint32_t y, Colour3& pixels, uint8_t face = 1)
 	{
 		assert(x <= width);
 		assert(y <= height);
 		assert(channels == 3 || channels == 4);
-		size_t pos = (x * channels) + (y * width);
-		data[pos] = pixel.r;
-		data[pos + 1] = pixel.g;
-		data[pos + 2] = pixel.b;
+		
+        if (face > 1 && type != ImageType::Cube)
+        {
+            LOGGER_ERROR("Trying to use a Image2D class a cube type when it has been initialised as a normal texture");
+            return {};
+        }
+        
+        size_t startPos = width * height * channels * (face - 1);
+        size_t pixelPos = ((x * channels) + (y * width)) + startPos;
+        assert(pixelPos < size);
+        
+		data[pixelPos] = pixels.r;
+		data[pixelPos + 1] = pixels.g;
+		data[pixelPos + 2] = pixels.b;
 	}
 
 	/**
@@ -190,7 +238,7 @@ public:
 	* @param y The y coord in pixels
 	* @param channel The channel to retrive (0 = red; 1 = green; 2 = blue; 3 = alpha)
 	*/
-	void setPixel(pixelType pixel, uint32_t x, uint32_t y, Colour4& pixels)
+	void setPixel(pixelType pixel, uint32_t x, uint32_t y, Colour4& pixels, uint8_t face = 1)
 	{
 		assert(x <= width);
 		assert(y <= height);
@@ -209,7 +257,17 @@ protected:
 	uint32_t width = 0;
 	uint32_t height = 0;
 	uint8_t channels = 0;
-	size_t size = 0;
-	uint8_t faces = 1;	//< set to 6 if a cube
+    uint8_t mipLevels = 1;
+    uint8_t faces = 1;    //< set to 6 if a cube
+	
+    // total size of image including all faces and mip maps
+    size_t size = 0;
+	
 };
+
+// useful types
+using Image2DU8 = Image2D<uint8_t>;
+using Image2DU16 = Image2D<uint16_t>;
+using Image2DF32 = Image2DL<float>;
+
 }    // namespace OmegaEngine
