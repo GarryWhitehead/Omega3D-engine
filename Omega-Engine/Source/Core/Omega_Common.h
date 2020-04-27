f448c3ff56984569569e745188bf294dfaac2db7
Garry Whitehead <garry@kudan.eu>
2020-04-27 08:09:17 +0100

    Added copyright header to all files

--- Omega-Engine/Source/Core/Omega_Common.h
+++ Omega-Engine/Source/Core/Omega_Common.h
@@ -1,10 +1,26 @@
+/* Copyright (c) 2018-2020 Garry Whitehead
+*
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the
+* "Software"), to deal in the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so, subject to
+* the following conditions:
+*
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #pragma once
 
 
-// sets whether omega engine should be executed in threaded mode - turning this off is mainly for debugging purposes!
-//#define OMEGA_ENGINE_THREADED 0
-
-// number of threads allocated to various tasks
-#define ASSET_LOAD_THREAD_COUNT 2
-#define SCENE_LOAD_THREAD_COUNT 2
 
